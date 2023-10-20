6cd3a8da5fa3b7437d7072a48a20286f8f2d77bb
musamM921 <musaartlove@gmail.com>
2023-10-21 01:09:24 +0400

    ifsucsses

--- fetch api/index.js
+++ fetch api/index.js
@@ -3,5 +3,10 @@ function showCountries() {
 
    xhr.open('GET', 'https://restcountries.com/v3.1/all', true)
 
+   xhr.onload = function() {
+    if (xhr.status == 200) {
+        console.log('sucsess')
+    }
+   }
    xhr.send()
 }
