66965e3fed89d8dd10928761c1aaf17e362db5a7
musamM921 <musaartlove@gmail.com>
2023-10-21 01:42:50 +0400

    result

--- fetch api/index.js
+++ fetch api/index.js
@@ -2,16 +2,24 @@ function showCountries() {
    let xhr =  new XMLHttpRequest()
 
    xhr.open('GET', 'https://restcountries.com/v3.1/all', true)
-
    xhr.onload = function() {
     if (xhr.status == 200) {
-        console.log('sucsess')
+        console.log('sucсess')
         let countries = JSON.parse(this.response)
         console.log(countries)
+
         countries.forEach(country => {
-           const countryCard = document.createElement('div')
-            countryCard.innerHTML = country.name
-        })
+            const countryCard = document.createElement('div');
+            const countryCardImage = document.createElement('img');
+        
+            const countryName = country.name.common;
+            const flagURL = country.flags.png;
+        
+            countryCard.innerHTML = countryName;
+            countryCardImage.src = flagURL;
+            countryCard.appendChild(countryCardImage);
+            document.getElementById('feed').appendChild(countryCard);
+        });
     }
    }
    xhr.send()
